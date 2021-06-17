50120b2f026fff02335c9749cfe284a3dfa0e88c
Jordan <19174130+jordan-git@users.noreply.github.com>
2021-06-17 15:38:51 +0100

    Removing old website stuff

--- server/app.py
+++
@@ -1,9 +0,0 @@
-# save this as app.py
-from flask import Flask, render_template
-
-app = Flask(__name__, static_folder="../client/build", static_url_path="/")
-
-
-@app.route("/")
-def index():
-    return app.send_static_file("index.html")
