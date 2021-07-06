bb56cd6bd050b96452b0f8fde52efb41ccad9bc7
KarlMcSherry <karldylan@hotmail.com>
2021-07-06 18:32:14 +0100

    Cleaned up code, started implementing GUI

--- bot/main.py
+++ bot/main.py
@@ -2,32 +2,29 @@ import os
 from binance.client import Client
 from binance.websockets import BinanceSocketManager
 from twisted.internet import reactor 
+from tkinter import *
+from tkinter import scrolledtext
 
 ## pip install python-binance==0.7.9 
 ## Installs appropriate python-binance version
 
+currentPrice = 0
+buyPrice = 0
+
+window = Tk()
+text =  scrolledtext.ScrolledText(window, width=40, height = 10)
+
 api_key = "0S6eRCHoDIrAc35nnMoWFzbiOJOhNmdVI7rM6roV872E7BfbF9u6ZBjYMp7uLFkd"
-
 api_secret = "McQE8un0xPY5GOtHTuBDBer7my5dFSqe5ffJ2gilKWGwEMn1SY8jE7sBQoOgBNRP"
-
 client = Client(api_key, api_secret)
-
 client.API_URL = 'https://testnet.binance.vision/api'
-
-global currentPrice;
-
-
 
 
 ''' 3 lines below print account balance of all assets, BTC balance in account, and USD balance in account'''
 #print(client.get_account())
+#print(client.get_asset_balance(asset='BTC'))
+#print(client.get_asset_balance(asset='USDT'))
 
-print(client.get_asset_balance(asset='BTC'))
-
-print(client.get_asset_balance(asset='USDT'))
-
-global buyPrice
-buyPrice = 0
 
 '''Code below opens a web socket and continually prints price of BTC'''
 btc_price = {'error':False}
@@ -36,16 +33,10 @@ def btc_price1(msg):
     if msg['e'] != 'error':
         global currentPrice
         currentPrice = msg['c'];
-     #   print(currentPrice);
-      #  testFunction()
-       
+        text.grid(column=0, row=0)
+        text.insert(INSERT, currentPrice + "\n")       
     else:
         btc_price['error'] = True
-
-sm = BinanceSocketManager(client)
-
-conn = sm.start_symbol_ticker_socket('BTCUSDT', btc_price1)
-sm.start()
 
 '''Function below exchanges USD for BTC'''
 def buyBTC():
@@ -62,17 +53,32 @@ def percentageDifference(currentPrice, buyPrice):
 
 '''Put buy/sell logic here'''
 def testFunction():
+        global buyPrice
         pDifference = percentageDifference(currentPrice, buyPrice)
         print(currentPrice)
         if  buyPrice == 0:
             buyBTC()
-            global buyPrice
             buyPrice = currentPrice
         elif pDifference >= .02 & buyPrice != 0:
             sellBTC()
         elif pDifference <= -0.05 & buyPrice != 0:
             sellBTC()
 
+def main():
+    print("Hello World")
+    window.title("StockUp")
+    window.geometry('400x270')
+
+    sm = BinanceSocketManager(client)
+    conn = sm.start_symbol_ticker_socket('BTCUSDT', btc_price1)
+    sm.start()
+
+    window.mainloop()
+
+
+
+if __name__ == "__main__":
+     main()
 
 
 
