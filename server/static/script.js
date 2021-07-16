f057b5194c00f542ca23be21f8ba763f36e079f6
Jordan <19174130+jordan-git@users.noreply.github.com>
2021-07-16 11:46:26 +0100

    Footer & some other style changes

--- server/static/script.js
+++ server/static/script.js
@@ -1,4 +1,8 @@
 const landingBtn = document.querySelector('#landing button');
+const contactBtn = document.querySelector('#contact-support button');
+
+const emailInput = document.querySelector('#email');
+const messageInput = document.querySelector('#message');
 
 // Scroll to specified element
 function scrollTo(element) {
@@ -8,4 +12,15 @@ function scrollTo(element) {
 // Scrolls from landing to the content
 landingBtn.addEventListener('click', () => {
   scrollTo(document.querySelector('.page:nth-child(2)'));
+});
+
+contactBtn.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  contactBtn.disabled = true;
+  emailInput.disabled = true;
+  messageInput.disabled = true;
+
+  alert("Your message has been submitted successfully. Expect a response within 2 working days.")
+
 });
